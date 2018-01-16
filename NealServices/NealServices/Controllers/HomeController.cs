3d34ad771bfacbec0138a7a4b885b1d3ecef98df
Bryant <wbryantcampbell@gmail.com>
2018-01-16 08:54:44 -0500

    init

---
+++ NealServices/NealServices/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using NealServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NealServices.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(EmailMessage model)
+        {
+             var smtp = new SmtpClient();
+
+            var body = "<p>Email From: {0} (phone: {1} email: {2} )</p><p>Message:</p><p>{3}</p>";
+            var mailMessage = new MailMessage();
+            mailMessage.To.Add(new MailAddress("nealservicesinc@hotmail.com"));
+            mailMessage.Subject = $"You have a message from NealServicesInc.com";
+            mailMessage.Body = string.Format(body, model.strFromName, model.strPhone, model.strFromAddress, model.strBody);
+            mailMessage.IsBodyHtml = true;
+
+            smtp.Send(mailMessage);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        //public ActionResult Testimony()
+        //{
+
+
+
+        //}
+    }
+}
---
+++ NealServices/NealServices/Models/EmailMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NealServices.Models
+{
+    public class EmailMessage
+    {
+
+        public string strFromAddress { get; set; }
+        public string strFromName { get; set; }
+        public string strPhone { get; set; }
+        public string strTo { get; set; }
+        public string strSubject { get; set; }
+        public string strBody { get; set; }
+    }
+}
